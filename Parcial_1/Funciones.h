066c38df6bf341f9f4b1e23a7d1c258ca775b12f
Cthulhu21 <62732060+Cthulhu21@users.noreply.github.com>
2023-04-18 22:14:06 -0500

    Mostrar Oferta
    Se muestra los horarios de cada materia

--- Parcial_1/Funciones.h
+++ Parcial_1/Funciones.h
@@ -5,11 +5,13 @@
 #endif // FUNCIONES_H
 
 
-void CargarMaterias(char Codigos[20][7], char Nombres[20][40], char *Teoricas, char *Independientes, char *Creditos);
+void CargarMaterias(char Codigos[20][7], char Nombres[20][40], char *Teoricas, char *Independientes, char *Creditos, char Horarios[20][40]);
 void Reiniciar(char* Arreglo, unsigned int Tamano);
 void CopiarA(char *Arreglo, char *Arreglo2, unsigned int Tamano);
+void GestionHorario(char Horario[7][24], char Codigos[20][7], char Nombres[20][40], char Creditos[20],  char Horarios[20][40]);
+void MostrarOferta(char Codigos[20][7], char Nombres[20][40],char Creditos[20],char Horarios[20][40]);
 
-void CargarMaterias(char Codigos[20][7], char Nombres[20][40], char *Teoricas, char *Independientes, char *Creditos)
+void CargarMaterias(char Codigos[20][7], char Nombres[20][40], char *Teoricas, char *Independientes, char *Creditos, char Horarios[20][40])
 {
     // Se leerá el archivo en formato Codigo:Nombre:Teoricas:Independientes:Creditos
     std::ifstream Archivo("Materias.txt");
@@ -21,7 +23,7 @@ void CargarMaterias(char Codigos[20][7], char Nombres[20][40], char *Teoricas, c
         }
         char Letra=0;
         unsigned int IteradorSegmentacion=0, Iterador=0, IteradorMaterias=0;;
-        char Codigo[7]={}, Nombre[40]={}, _Teoricas, _Independientes, _Creditos;
+        char Codigo[7]={}, Nombre[40]={}, _Teoricas, _Independientes, _Creditos, Horario[40]={};
         while(Archivo>>std::noskipws >>Letra)
         {
             if(Letra=='\n')
@@ -29,12 +31,14 @@ void CargarMaterias(char Codigos[20][7], char Nombres[20][40], char *Teoricas, c
                 IteradorSegmentacion=0;
                 CopiarA(Codigo, Codigos[IteradorMaterias], 7);
                 CopiarA(Nombre, Nombres[IteradorMaterias],40);
+                CopiarA(Horario, Horarios[IteradorMaterias],40);
                 *(Teoricas+IteradorMaterias)=_Teoricas;
                 *(Independientes+IteradorMaterias)=_Independientes;
                 *(Creditos+IteradorMaterias)=_Creditos;
 
                 Reiniciar(Codigo, 7);
-                Reiniciar(Nombre, 30);
+                Reiniciar(Nombre, 40);
+                Reiniciar(Horario, 40);
                 _Teoricas=0;
                 _Independientes=0;
                 _Creditos=0;
@@ -69,6 +73,11 @@ void CargarMaterias(char Codigos[20][7], char Nombres[20][40], char *Teoricas, c
             else if(IteradorSegmentacion==4)
             {
                 _Creditos=Letra;
+                Iterador+=1;
+            }
+            else
+            {
+                *(Horario+Iterador)=Letra;
                 Iterador+=1;
             }
         }
@@ -118,4 +127,68 @@ void CalculoIndependientes(char *Teoricas, char *Independientes, char *Creditos)
         unsigned int HorasTeoricas=Teoricas[i]-48;
         *(Independientes+i)=Horas-HorasTeoricas+48;
     }
+}
+
+void GestionHorario(char Horario[7][24], char Codigos[20][7], char Nombres[20][40], char Creditos[20], char Horarios[20][40])
+{
+    using namespace std;
+    {
+        std::cout<<"A continuacion se le mostraran las materias ofertadas y usted elegira cuales desea cursar 1 a 1" << std::endl;
+        char Entrada[2]={};
+        do
+        {
+            MostrarOferta(Codigos, Nombres, Creditos, Horarios);
+            cout<< "Ingrese el numero de la materia que desea registrar en su horario (0 para salir)" << endl;
+            cin >> Entrada;
+        }while(Entrada[0]==true);
+    }
+}
+
+void MostrarOferta(char Codigos[20][7], char Nombres[20][40],char Creditos[20], char Horarios[20][40])
+{
+    using namespace std;
+    {
+        cout << "Numero\tCodigo\t\tNombre\t\t\t\t\tCreditos\n";
+        unsigned int Tamano=0;
+        for(int i=0; i<20;i++)
+        {
+            if(Codigos[i][0]=='\0')
+            {
+                break;
+            }
+            Tamano+=1;
+        }
+        for(unsigned int i=0; i<Tamano;i++) //Mostrar codigos
+        {
+            cout <<i+1<<"\t";
+            for(int j=0; j<7; j++)
+            {
+                cout << Codigos[i][j];
+            }
+            cout <<"\t\t";
+            for(int j=0; j<40; j++) //Mostrar Nombres
+            {
+                cout << Nombres[i][j];
+                if(Nombres[i][j]=='0')
+                {
+                    cout<<"\t";
+                    break;
+                }
+            }
+            cout << Creditos[i]; //Mostrar Creditos
+            for(int j=0; j<40; j++)
+            {
+                if(Horarios[i][j]=='\0')
+                {
+                    break;
+                }
+                if(Horarios[i][j]>64 and Horarios[i][j]<91)
+                {
+                    cout << "\t";
+                }
+                cout << Horarios[i][j];
+            }
+            cout << endl;
+        }
+    };
 }
--- Parcial_1/main.cpp
+++ Parcial_1/main.cpp
@@ -4,8 +4,10 @@ using namespace std;
 
 int main()
 {
-    char Codigos[20][7]={}, Nombres[20][40]={}, Teoricas[20]={}, Independientes[20]={}, Creditos[20]={};
-    CargarMaterias(Codigos, Nombres, Teoricas, Independientes, Creditos);
+    char Codigos[20][7]={}, Nombres[20][40]={}, Teoricas[20]={}, Independientes[20]={}, Creditos[20]={}, Horarios[20][40]={};
+    CargarMaterias(Codigos, Nombres, Teoricas, Independientes, Creditos, Horarios);
     CalculoIndependientes(Teoricas, Independientes, Creditos);
+    char Horario[7][24]={};
+    GestionHorario(Horario, Codigos, Nombres, Creditos, Horarios);
     return 0;
 }
